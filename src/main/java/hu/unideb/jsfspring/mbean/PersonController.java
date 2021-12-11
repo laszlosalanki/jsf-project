2326045189f1fb96ff0e3827400a2b95c6569a00
László Salánki <laszlosalanki2001@gmail.com>
2021-12-11 22:56:29 +0100

    details view corrected

--- src/main/java/hu/unideb/jsfspring/mbean/PersonController.java
+++ src/main/java/hu/unideb/jsfspring/mbean/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.annotation.SessionScope;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller("personController")
 @SessionScope
@@ -30,14 +31,39 @@ public class PersonController {
         if (!this.getPersonUITOList().isEmpty()) {
             this.getPersonUITOList().clear();
         }
+        if (!this.getFoodUITOList().isEmpty())
+            this.getFoodUITOList().clear();
         System.out.println(" >>>>>>>>>>>>> " + personService);
         this.getPersonUITOList().addAll(personService.getAllPerson());
+        this.getFoodUITOList().addAll(foodService.doFetchAllFood());
+    }
+
+    public List<FoodUITO> getFoodByPerson(Long id) {
+        getAllPerson();
+        List<FoodUITO> foodList = new ArrayList<>();
+        for(FoodUITO f : foodUITOList)
+            if (f.getPersonUITO().getPersonId().equals(id))
+                foodList.add(f);
+        return foodList;
+    }
+
+    public List<FoodUITO> getFoodUITOList() {
+        if (foodUITOList == null) {
+            foodUITOList = new ArrayList<>();
+        }
+
+        return foodUITOList;
+    }
+
+    public void setFoodUITOList(List<FoodUITO> foodUITOList) {
+        this.foodUITOList = foodUITOList;
     }
 
     public List<PersonUITO> getPersonUITOList() {
         if (personUITOList == null) {
             personUITOList = new ArrayList<>();
         }
+
         return personUITOList;
     }
 
--- src/main/webapp/WEB-INF/classes/data.sql
+++ src/main/webapp/WEB-INF/classes/data.sql
@@ -1,5 +1,5 @@
 insert into PEOPLE(PERSON_ID, PERSON_NAME, PERSON_AGE) values(10, 'Proba Peter', 20);
 insert into PEOPLE(PERSON_ID, PERSON_NAME, PERSON_AGE) values(20, 'Har Monika', 25);
 
-insert into FOOD_DIARY(food_Id, FOOD_NAME, GRAMS, CALORIES_PER_GRAM, EAT_TIME, PERSON_ID) values(1, 'Keksz', 100, 5, 24523412, 10);
-insert into FOOD_DIARY(food_Id, FOOD_NAME, GRAMS, CALORIES_PER_GRAM, EAT_TIME, PERSON_ID) values(2, 'Muzli', 80, 15, 24633416, 20);
+insert into FOOD_DIARY(food_Id, FOOD_NAME, GRAMS, CALORIES_PER_GRAM, EAT_TIME, PERSON_ID) values(100, 'Keksz', 100, 5, 24523412, 10);
+insert into FOOD_DIARY(food_Id, FOOD_NAME, GRAMS, CALORIES_PER_GRAM, EAT_TIME, PERSON_ID) values(200, 'Muzli', 80, 15, 24633416, 20);
